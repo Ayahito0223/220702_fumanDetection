846444dbca82291651ce64243503c2f23af67f8e
Ayahito0223 <Engineer.FumiM@gmail.com>
2022-07-06 00:13:19 +0900

    arduinoとpythonのやり取り部分の実装完了 後は、人感センサーで反応させて、slackに通知、そしてブザーを鳴らす

--- client/client.ino
+++ client/client.ino
@@ -1,14 +1,178 @@
-#include "Ultrasonic.h"
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+#define PIR_MOTION_SENSOR 4
 
-Ultrasonic ultrasonic(7);
+#include "arduino_secrets.h"
+
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+String arduino_pass = SECRET_ARDUINO_PASS;
+int status = WL_IDLE_STATUS;
+
+WiFiUDP Udp;
+unsigned int localPort = 2390;
+
+char packetBuffer[1024]; //buffer to hold incoming packet
+
+int counter = 0;
+
 void setup() {
+  pinMode(PIR_MOTION_SENSOR, INPUT);
+
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  initWifi();
+  initUser();
+  Serial.println("Start Detection");
 }
 
-long RangeInCentimeters;
 void loop() {
-  RangeInCentimeters = ultrasonic.MeasureInCentimeters();
-  Serial.print(RangeInCentimeters);
-  Serial.println(" cm");
-  delay(1000);
+  if (WiFi.status() == 6) {
+    status = 6;
+    initWifi();
+    initUser();
+    Serial.println("Start Detection");
+  }
+
+  if (digitalRead(PIR_MOTION_SENSOR)) {
+    counter += 1;
+    if (counter >= 10) {
+      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+      Udp.write("2");
+      Serial.println("human detected!");
+      Udp.endPacket();
+      counter = 0;
+    }
+  } else {
+    counter = 0;
+  }
+
+  delay(100);
+}
+
+void initWifi() {
+  Udp.stop();
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    delay(5000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+
+  Udp.begin(localPort);
+}
+
+void initUser() {
+  bool findServer = false;
+  while (!findServer) {
+    int packetSize = Udp.parsePacket();
+    if (packetSize) {
+      Serial.print("Received packet of size ");
+      Serial.println(packetSize);
+      Serial.print("From ");
+      IPAddress remoteIp = Udp.remoteIP();
+      Serial.print(remoteIp);
+      Serial.print(", port ");
+      Serial.println(Udp.remotePort());
+
+      // read the packet into packetBufffer
+      int len = Udp.read(packetBuffer, 1023);
+      if (len > 0) {
+        packetBuffer[len] = 0;
+      }
+      Serial.print("Contents:");
+      Serial.println(packetBuffer);
+
+      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+      if (arduino_pass.equals(String(packetBuffer))) {
+        Udp.write("1");
+        Serial.println("valid server!");
+        findServer = true;
+      } else {
+        Udp.write("0");
+        Serial.println("Invalid server");
+      }
+      Udp.endPacket();
+      Serial.println("");
+    }
+  }
+  delay(5000);
+}
+
+void printWifiData() {
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print port
+  Serial.print("Listening Port: ");
+  Serial.println(localPort);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  printMacAddress(mac);
+  Serial.println("");
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
 }
--- server/main.py
+++ server/main.py
@@ -1,5 +1,8 @@
 import serial
+import socket
 import os
+import ipaddress
+import hashlib
 
 from dotenv import load_dotenv
 from slack import SlackAPI
@@ -8,29 +11,68 @@ def main():
   load_dotenv()
   SLACK_BOT_USER_OAUTH_TOKEN = os.getenv('SLACK_BOT_USER_OAUTH_TOKEN')
 
-  slack = SlackAPI(SLACK_BOT_USER_OAUTH_TOKEN)
+  M_SIZE = 1024
 
-  ser = serial.Serial('COM5', 9600)
   while True:
-    print('データをslackに送信する場合は、y')
-    print('プログラムを止める場合は、q')
-    userInput = input('> ')
-    if userInput == 'y':
-      val_arduino = ser.readline()
-      val_decoded = repr(val_arduino.decode())[1:-5]
-      result = slack.sendMessage(val_decoded)
-      if result.get('ok'):
-        print('「 {} 」が無事送信されました。'.format(val_decoded))
-      else:
-        print('エラーが起きたようです。')
-        print(result)
-    elif userInput == 'q':
+    try:
+      arduino_ip_address_str = input("arduinoのIPアドレスを入力してください> ")
+      arduino_ip_address = str(ipaddress.ip_address(arduino_ip_address_str))
+      break
+    except ValueError:
+      print('ipアドレスではありません。')
+      print('')
+  
+  while True:
+    try:
+      arduino_port = int(input("arduinoのポートを入力してください> "))
+      break
+    except ValueError:
+      print("数字で入力してください。")
+      print('')
+
+  arduino_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+  while True:
+    arduino_pass = input("arduinoに接続するためのパスワードを入力してください。> ")
+    arduino_socket.sendto(
+      str(hashlib.sha256(arduino_pass.encode()).hexdigest()).encode('utf-8'), 
+      (arduino_ip_address, arduino_port)
+    )
+    print('Arduinoからのレスポンスを待っています。。。。')
+    message, addr = arduino_socket.recvfrom(M_SIZE)
+    print(message.decode(encoding='utf-8'))
+    if message.decode(encoding='utf-8') == '1':
+      print("接続に成功しました。")
       break
     else:
-      print('入力内容がおかしいです。')
-      print()
-  
-  ser.close()
+      print("接続に失敗しました。")
+      print('')
+
+  slack = SlackAPI(SLACK_BOT_USER_OAUTH_TOKEN)
+
+  while True:
+    message, addr = arduino_socket.recvfrom(M_SIZE)
+    if message.decode(encoding='utf-8') == '2':
+      print("detected!")
+
+  # while True:
+  #   print('データをslackに送信する場合は、y')
+  #   print('プログラムを止める場合は、q')
+  #   userInput = input('> ')
+  #   if userInput == 'y':
+  #     val_arduino = ser.readline()
+  #     val_decoded = repr(val_arduino.decode())[1:-5]
+  #     result = slack.sendMessage(val_decoded)
+  #     if result.get('ok'):
+  #       print('「 {} 」が無事送信されました。'.format(val_decoded))
+  #     else:
+  #       print('エラーが起きたようです。')
+  #       print(result)
+  #   elif userInput == 'q':
+  #     break
+  #   else:
+  #     print('入力内容がおかしいです。')
+  #     print()
 
 if __name__ == "__main__":
   main()
