e9a397cb6d810fe01e085f0ccaa50a927a05038b
Ayahito0223 <Engineer.FumiM@gmail.com>
2022-07-09 03:03:37 +0900

    とりあえず暗号化通信をしてみた。 次出来そうなことは、違う端末から勝手にアクセスされてしまった時に、今のままだと、暗号の文字列が勝手に進んでいってしまう

--- client/client.ino
+++ client/client.ino
@@ -2,28 +2,65 @@
 #include <WiFiNINA.h>
 #include <WiFiUdp.h>
 #include <Wire.h>
-
-#define PIR_MOTION_SENSOR 4
-#define BUZZER 3
+#include <Crypto.h>
+#include <SHA256.h>
+#include <string.h>
 
 #include "arduino_secrets.h"
 #include "rgb_lcd.h"
 
+// MOTION SENSORのpinは4
+#define PIR_MOTION_SENSOR 4
+
+// BUZZERのpinは3
+#define BUZZER 3
+
+// SSID
 char ssid[] = SECRET_SSID;
+
+// SSIDのパスワード
 char pass[] = SECRET_PASS;
+
+// Arduinoからデータを受け取るためのパスワード
 String arduino_pass = SECRET_ARDUINO_PASS;
-int status = WL_IDLE_STATUS;
 
-rgb_lcd lcd;
-
-WiFiUDP Udp;
+// データを受け取るポート番号
 unsigned int localPort = 2390;
 
-char packetBuffer[1024]; //buffer to hold incoming packet
+// WiFiのステータスを管理する変数
+int status = WL_IDLE_STATUS;
 
+// UDPを使うための変数
+WiFiUDP Udp;
+
+// UDPで送られて来たデータを入れるための変数
+char packetBuffer[1024];
+
+// LCDを使うための変数
+rgb_lcd lcd;
+
+// 暗号通信をするためのpythonから送られて来たランダムな文字列を入れる変数
+const int STREAM_PASS_SIZE = 32;
+char streamPass[STREAM_PASS_SIZE];
+int streamPassCounter = 0;
+
+// 人を検知しすぎないように制御するカウンター
 int counter = 0;
 
+// 処理を制御する変数
+int processNum = 0;
+
+// SHA256でハッシュ化するための変数
+SHA256 sha256;
+
+// DEBUG用のフラグ（Serialの内容を読みたい時Trueに、Arudinoを独立して動かす時にはFalse）
 bool DEBUG = false;
+
+#define USER_INVALID "0"
+#define USER_VALID "1"
+#define HUMAN_DETECTED "2"
+#define SERVER_OK "5"
+#define SERVER_QUIT "9"
 
 void setup() {
   pinMode(BUZZER, INPUT);
@@ -37,102 +74,171 @@ void setup() {
     if (!DEBUG) {
       break;
     }
-    ; // wait for serial port to connect. Needed for native USB port only
+    ;
   }
-  while (true) {
-    initWifi();
-    if (!initUser()) {
-      status = 6;
-      continue;
-    }
-    break;
-  }
-  Serial.println("Start Detection");
-  lcd.clear();
-  lcd.setRGB(127, 255, 127);
-  lcd.print("start detect!");
-
-  delay(5000);
+//  Serial.println((2 + 1) % 3);
 }
 
 void loop() {
+  // 必ずやる処理
   if (WiFi.status() == 6) {
-    digitalWrite(BUZZER, LOW);
-    status = 6;
-    while (true) {
+    processNum = 0;
+  }
+
+  switch (processNum) {
+    //wifiのinit処理
+    case 0:
+      digitalWrite(BUZZER, LOW);
+      status = 6;
       initWifi();
+
+      processNum = 1;
+
+      break;
+
+    // ユーザーのinit処理
+    case 1:
+      digitalWrite(BUZZER, LOW);
       if (!initUser()) {
-        status = 6;
-        continue;
+        processNum = 0;
+
+      } else {
+        Serial.println("Start Detection");
+        lcd.clear();
+        lcd.setRGB(127, 255, 127);
+        lcd.print("start detect!");
+
+        processNum = 2;
+        delay(2500);
       }
       break;
-    }
-    Serial.println("Start Detection");
-    lcd.clear();
-    lcd.setRGB(127, 255, 127);
-    lcd.print("start detect!");
 
-    delay(5000);
-  }
+    // メインの処理
+    case 2:
+      // 人を検知してpythonの方に送る
+      if (digitalRead(PIR_MOTION_SENSOR)) {
+        counter += 1;
+        if (counter >= 10) {
+          lcd.clear();
+          lcd.print("human detect!");
+          lcd.setRGB(255, 127, 127);
+          digitalWrite(BUZZER, HIGH);
+          Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+          Udp.write(useHashMessage(HUMAN_DETECTED));
+          Serial.println("human detected!");
+          Udp.endPacket();
+          counter = 0;
 
-  if (digitalRead(PIR_MOTION_SENSOR)) {
-    counter += 1;
-    if (counter >= 10) {
-      lcd.clear();
-      lcd.print("human detect!");
-      lcd.setRGB(255, 127, 127);
-      digitalWrite(BUZZER, HIGH);
-      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-      Udp.write("2");
-      Serial.println("human detected!");
-      Udp.endPacket();
-      counter = 0;
-    }
-  } else {
-    lcd.clear();
-    lcd.setRGB(127, 255, 127);
-    lcd.print("detecting...");
-    digitalWrite(BUZZER, LOW);
-    counter = 0;
-  }
+          delay(1000);
+          int quitCount = 0;
+          while (true) {
+            if (WiFi.status() == 6) {
+              processNum = 0;
+              break;
+            }
+            if (quitCount >= 40000) {
+              processNum = 1;
+              break;
+            }
+            int packetSize = Udp.parsePacket();
+            if (packetSize) {
+              Serial.print("Received packet of size ");
+              Serial.println(packetSize);
+              Serial.print("From ");
+              IPAddress remoteIp = Udp.remoteIP();
+              Serial.print(remoteIp);
+              Serial.print(", port ");
+              Serial.println(Udp.remotePort());
 
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
-    Serial.print("Received packet of size ");
-    Serial.println(packetSize);
-    Serial.print("From ");
-    IPAddress remoteIp = Udp.remoteIP();
-    Serial.print(remoteIp);
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
+              // read the packet into packetBufffer
+              int len = Udp.read(packetBuffer, 1023);
+              if (len > 0) {
+                packetBuffer[len] = 0;
+              }
+              Serial.print("Contents:");
+              Serial.println(packetBuffer);
 
-    // read the packet into packetBufffer
-    int len = Udp.read(packetBuffer, 1023);
-    if (len > 0) {
-      packetBuffer[len] = 0;
-    }
-    Serial.print("Contents:");
-    Serial.println(packetBuffer);
-
-    if (String(packetBuffer).equals("3")) {
-      while (true) {
-        initWifi();
-        if (!initUser()) {
-          status = 6;
-          continue;
+              if (String(packetBuffer).equals(checkHashMessage(SERVER_OK))) {
+                break;
+              }
+            }
+            quitCount += 1;
+          }
         }
-        break;
+      } else {
+        lcd.clear();
+        lcd.setRGB(127, 255, 127);
+        lcd.print("detecting...");
+        digitalWrite(BUZZER, LOW);
+        counter = 0;
+
+        // pythonからのデータを受け取る。
+        int packetSize = Udp.parsePacket();
+        if (packetSize) {
+          Serial.print("Received packet of size ");
+          Serial.println(packetSize);
+          Serial.print("From ");
+          IPAddress remoteIp = Udp.remoteIP();
+          Serial.print(remoteIp);
+          Serial.print(", port ");
+          Serial.println(Udp.remotePort());
+
+          // read the packet into packetBufffer
+          int len = Udp.read(packetBuffer, 1023);
+          if (len > 0) {
+            packetBuffer[len] = 0;
+          }
+          Serial.print("Contents:");
+          Serial.println(packetBuffer);
+
+          if (String(packetBuffer).equals(checkHashMessage(SERVER_QUIT))) {
+            processNum = 1;
+          }
+        }
       }
-      Serial.println("Start Detection");
-      lcd.clear();
-      lcd.setRGB(127, 255, 127);
-      lcd.print("start detect!");
 
-      delay(5000);
-    }
+      delay(100);
+
+      break;
   }
+}
 
-  delay(100);
+#define HASH_SIZE 32
+#define BLOCK_SIZE 64
+char hex[256];
+byte buffer[128];
+
+char *btoh(char *dest, uint8_t *src, int len) {
+ char *d = dest;
+ while( len-- ) sprintf(d, "%02x", (unsigned char)*src++), d += 2;
+ return dest;
+}
+
+char* hashed(Hash *hash, const char* hashvalue)
+{
+  uint8_t value[HASH_SIZE];
+
+  hash->reset();
+  hash->update(hashvalue, strlen(hashvalue));
+  hash->finalize(value, sizeof(value));
+  
+  return btoh(hex, value, 32);
+}
+
+char* checkHashMessage(char* original) {
+  String num = original;
+  String pass = (String)streamPass[streamPassCounter];
+  String str = num + pass;
+  streamPassCounter = (streamPassCounter + 1) % STREAM_PASS_SIZE;
+  return hashed(&sha256, str.c_str());
+}
+
+char* useHashMessage(char* message) {
+  String num = message;
+  String pass = (String)streamPass[streamPassCounter];
+  String str = num + pass;
+  streamPassCounter = (streamPassCounter + 1) % STREAM_PASS_SIZE;
+  return hashed(&sha256, str.c_str());
 }
 
 void initWifi() {
@@ -174,13 +280,16 @@ void initWifi() {
 }
 
 bool initUser() {
-  bool findServer = false;
+  bool findUser = false;
+
+  Serial.println("wait valid user");
+
   lcd.clear();
   lcd.setRGB(255, 255, 127);
   lcd.print("wait valid user");
   lcd.setCursor(0, 1);
   lcd.print(WiFi.localIP());
-  while (!findServer) {
+  while (!findUser) {
     if (WiFi.status() == 6) {
       return false;
     }
@@ -204,15 +313,43 @@ bool initUser() {
 
       Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
       if (arduino_pass.equals(String(packetBuffer))) {
-        Udp.write("1");
-        Serial.println("valid server!");
-        findServer = true;
+        Udp.write(USER_VALID);
+        findUser = true;
       } else {
-        Udp.write("0");
+        Udp.write(USER_INVALID);
         Serial.println("Invalid server");
       }
       Udp.endPacket();
       Serial.println("");
+    }
+  }
+  delay(1500);
+  while (true) {
+    if (WiFi.status() == 6) {
+      return false;
+    }
+    int packetSize = Udp.parsePacket();
+    if (packetSize) {
+      Serial.print("Received packet of size ");
+      Serial.println(packetSize);
+      Serial.print("From ");
+      IPAddress remoteIp = Udp.remoteIP();
+      Serial.print(remoteIp);
+      Serial.print(", port ");
+      Serial.println(Udp.remotePort());
+
+      int len = Udp.read(streamPass, STREAM_PASS_SIZE);
+      if (len > 0) {
+        streamPass[len] = 0;
+      }
+      Serial.print("Contents:");
+      Serial.println(streamPass);
+      Serial.println(strlen(streamPass));
+      streamPassCounter = 0;
+
+      Serial.println("valid server!");
+      Serial.println("");
+      break;
     }
   }
   return true;
--- server/main.py
+++ server/main.py
@@ -3,20 +3,48 @@ import os
 import ipaddress
 import hashlib
 import threading
+import random
+import string
+import time
 
 from dotenv import load_dotenv
 from slack import SlackAPI
 
+USER_INVALID = '0'
+USER_VALID = '1'
+HUMAN_DETECTED = '2'
+SERVER_OK = '5'
+SERVER_QUIT = '9'
+
 M_SIZE = 1024
 arduino_socket = None
+arduino_ip_address = None
+arduino_port = None
+streamPass = ''
+streamPassCounter = 0
+STREAM_PASS_SIZE = 32
+
 slack = None
 
+def checkHashMessage(original):
+  global streamPass, streamPassCounter
+  result = hashlib.sha256((original + streamPass[streamPassCounter]).encode()).hexdigest()
+  streamPassCounter = (streamPassCounter + 1) % STREAM_PASS_SIZE
+  return result
+
+def useHashMassage(message):
+  global streamPass, streamPassCounter
+  result = hashlib.sha256((message + streamPass[streamPassCounter]).encode()).hexdigest().encode('utf-8')
+  streamPassCounter = (streamPassCounter + 1) % STREAM_PASS_SIZE
+  return result
+
 def arduinoResponce():
-  global M_SIZE, arduino_socket, slack
+  global M_SIZE, arduino_socket, slack, arduino_ip_address, arduino_port
   counter = 1
   while True:
     message, addr = arduino_socket.recvfrom(M_SIZE)
-    if message.decode(encoding='utf-8') == '2':
+    if message.decode(encoding='utf-8') == checkHashMessage(HUMAN_DETECTED):
+      arduino_socket.sendto(useHashMassage(SERVER_OK), (arduino_ip_address, arduino_port))
       if counter == 1:
         result = slack.sendMessage("human detected!!!!!!!")
         if result.get('ok'):
@@ -29,8 +57,9 @@ def arduinoResponce():
       counter += 1
 
 def main():
-  global M_SIZE, arduino_socket, slack
+  global M_SIZE, arduino_socket, slack, streamPass, arduino_ip_address, arduino_port
 
+  print(hashlib.sha256('abc'.encode()).hexdigest())
   load_dotenv()
   SLACK_BOT_USER_OAUTH_TOKEN = os.getenv('SLACK_BOT_USER_OAUTH_TOKEN')
 
@@ -61,7 +90,12 @@ def main():
     )
     print('arduinoからのレスポンスを待っています。。。。')
     message, addr = arduino_socket.recvfrom(M_SIZE)
-    if message.decode(encoding='utf-8') == '1':
+    if message.decode(encoding='utf-8') == USER_VALID:
+      randlst = [random.choice(string.ascii_letters + string.digits) for i in range(STREAM_PASS_SIZE)]
+      streamPass = ''.join(randlst)
+      print(streamPass)
+      time.sleep(2)
+      arduino_socket.sendto(streamPass.encode('utf-8'), (arduino_ip_address, arduino_port))
       print("接続に成功しました。")
       break
     else:
@@ -74,9 +108,13 @@ def main():
 
   print("プログラムを終わる場合は、q")
   while True:
-    mes = input()
-    if mes == 'q':
-      arduino_socket.sendto('3'.encode('utf-8'), (arduino_ip_address, arduino_port))
+    try:
+      mes = input()
+      if mes == 'q':
+        arduino_socket.sendto(useHashMassage(SERVER_QUIT), (arduino_ip_address, arduino_port))
+        exit()
+    except KeyboardInterrupt:
+      arduino_socket.sendto(useHashMassage(SERVER_QUIT), (arduino_ip_address, arduino_port))
       exit()
 
 if __name__ == "__main__":
